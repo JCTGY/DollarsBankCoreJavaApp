280294300798ce35b26b4a27a9925aea8923e915
JCTGY <fan3510@gmail.com>
2021-01-07 15:40:16 -0800

    Add color using Janis and anis plugin for Eclipse

--- src/com/jump/plus/DollarBankRunner.java
+++ src/com/jump/plus/DollarBankRunner.java
@@ -5,7 +5,7 @@ import com.jump.plus.controller.ConsoleController;
 public class DollarBankRunner {
 
 	public static void main(String[] args) {
-		
+		System.setProperty("jansi.passthrough", "true");
 		ConsoleController consoleController = new ConsoleController();
 		consoleController.startConsole();
 	}
--- src/com/jump/plus/controller/ConsoleController.java
+++ src/com/jump/plus/controller/ConsoleController.java
@@ -1,22 +1,24 @@
 package com.jump.plus.controller;
 
-import java.io.Console;
 import java.util.Scanner;
+
+import org.fusesource.jansi.AnsiConsole;
+import static org.fusesource.jansi.Ansi.*;
+import static org.fusesource.jansi.Ansi.Color.*;
 
 import com.jump.plus.constant.Command;
 import com.jump.plus.view.ConsoleScanner;
 import com.jump.plus.view.DollarBankView;
-import com.mysql.cj.protocol.Message;
 
 public class ConsoleController {
 
 	
 	private Scanner scanner = ConsoleScanner.getScanner();
 	private DollarBankView dbView = new DollarBankView();
-
 	public void startConsole() {
 		dbView.printHeader();
-		
+		AnsiConsole.systemInstall();
+		String[] inputValue = null;
 		UserController userController = new UserController();
 		while (true) {
 			String message = scanner.next();
@@ -24,12 +26,14 @@ public class ConsoleController {
 			if (message.equalsIgnoreCase(Command.EXIT.name())) {
 				break ;
 			} else if (message.equalsIgnoreCase(Command.LOGIN.name())) {
-				String[] inputValue = inputLogin();
+				inputValue = inputLogin();
 				if (inputValue != null && userController.login(inputValue[0], inputValue[1])) {
-					System.out.println("User login");
+					System.out.println(ansi().eraseScreen().fg(GREEN).a("User login").reset());
+					inputValue = null;
 				}
 			} else if (message.equalsIgnoreCase(Command.LOGOUT.name())) {
-				
+				userController.logout();
+				System.out.println(ansi().eraseScreen().fg(GREEN).a("User logout").reset());
 			}
 		}
 	}
@@ -40,15 +44,16 @@ public class ConsoleController {
 		String message = null;
 		
 		System.out.println("Enter BACK to go back to Main View");
-		System.out.println("Enter Username: \n");
+		System.out.println("Enter Username: ");
 		while (true) {
 			if (loginValue[0] == null && message != null) {
 				loginValue[0] = message;
-				System.out.println("Enter Password: \n");
+				System.out.println("Enter Password: ");
 			} else if (loginValue[1] == null) {
 				loginValue[1] = message;
-			} else return loginValue;
-			
+			} 
+			if (loginValue[0] != null && loginValue[1] != null)
+				return loginValue;
 			message = scanner.next();
 			if (message.equalsIgnoreCase(Command.BACK.name())) return null;
 			
